a23e21acfcd5b9dff4c802af7f0185403fdd53f5
Dupe Fadina <maafadina@hotmail.com>
2019-07-26 23:59:44 -0700

    Set up the model and controller part of the applicaton

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
+require("dotenv").config();
 var mysql = require("mysql");
-
 var connection = mysql.createConnection({
-    host: "",
+    host: process.env.MYSQL_HOST,
     port: 3306,
-    user: "",
-    password: "",
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
     database: "burgers_db"
   });
 
--- config/orm.js
+++ config/orm.js
@@ -1,25 +1,69 @@
 var connection = require("./connection.js");
 
+//Helper function to print question marks("?,?")
+function printQuestMarks(arrLength){
+    var questArr = [];
+    for (var i = 0; i < arrLength; i++) {
+        //build array of quest marks based on parameter
+        questArr.push("?");
+    }
+    return questArr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+  
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+      var value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+        // e.g. {sleepy: true} => ["sleepy=true"]
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+  }
+  
 var orm = {
-    selectAll: function() {
+    selectAll: function(tableInput, cb) {
       var queryString = "SELECT * FROM ??";
       connection.query(queryString, [], function(err, result) {
         if (err) throw err;
-        console.log(result);
+        cb(result);
       });
     },
-    InsertOne: function() {
-      var queryString = "";
+    InsertOne: function(tableInput, cols, vals, cb) {
+      var queryString = "INSERT INTO " + tableInput;
+      queryString += "(";
+      queryString += cols.toString();
+      queryString += ") ";
+      queryString += "VALUES(";
+      queryString += printQuestMarks(vals.length);
+      queryString += ")";
       console.log(queryString);
-      connection.query(queryString, [], function(err, result) {
+      connection.query(queryString, vals, function(err, result) {
         if (err) throw err;
         console.log(result);
       });
     },
-    updateOne: function(){
-      var queryString = "";
+    updateOne: function(tableInput, objColVals, condition){
+      var queryString = "UPDATE " + tableInput + " SET ";
+      queryString += objToSql(objColVals);
+      queryString += " WHERE ";
+      queryString += condition;
+  
+  
          
-      connection.query(queryString, [], function(err, result) {
+      connection.query(queryString, function(err, result) {
           if (err) throw err;
           console.log(result);
         }
