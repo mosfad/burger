4aff814ccfb4e74dfce259611bd0f240df7b1d29
Dupe Fadina <maafadina@hotmail.com>
2019-07-29 21:15:26 -0700

    Setting the MYSQL connections for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 var mysql = require("mysql");
 var connection;
-/*
+
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
@@ -12,15 +12,16 @@ if (process.env.JAWSDB_URL) {
     password: "hacktheplanet",
     database: "todoagain_db"
   });
-}*/
+}
 
+/*
 var connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     port: 3306,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: "burgers_db"
-  });
+  });*/
 
 connection.connect(function(err) {
   if (err) {
